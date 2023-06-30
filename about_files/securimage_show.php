01e24a34e6ce8f085b411e288d9fceb149fe8142
corbin muraro <corbinmuraro@gmail.com>
2023-06-30 15:46:09 +0200

    sidebar cleanup, some file reorg

--- about_files/securimage_show.php
+++
@@ -1,2 +0,0 @@
-<br />
-<b>Fatal error</b>:  Array and string offset access syntax with curly braces is no longer supported in <b>/Users/trw/Documents/4HMT Documents/Website/Website code 040723/4HMillionTrees/4trees/securimage/securimage.php</b> on line <b>931</b><br />
